aebc6bd9cc20c459ce7b88ae126e4bd187292115
Samuel Plumppu <greenheartmusic@gmail.com>
2019-10-13 23:01:36 +0200

    Updates

---
+++ precache-manifest.2b1080ec62c8c5e1ef17d3b6dc7dc728.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48072a28709b62cd3858a5f269f96fdc",
+    "url": "/green-choices/index.html"
+  },
+  {
+    "revision": "efd5042cdb7356cd0295",
+    "url": "/green-choices/static/css/main.0b28b1d8.chunk.css"
+  },
+  {
+    "revision": "197a2f48289667bafbdb",
+    "url": "/green-choices/static/js/2.d4609273.chunk.js"
+  },
+  {
+    "revision": "efd5042cdb7356cd0295",
+    "url": "/green-choices/static/js/main.2574cee8.chunk.js"
+  },
+  {
+    "revision": "1a0d47b25b20956c23d6",
+    "url": "/green-choices/static/js/runtime-main.8213fbf1.js"
+  }
+]);
