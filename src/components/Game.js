d7ea5d95504bf4014a9a0ab527e5c9e3b6add488
Samuel Plumppu <greenheartmusic@gmail.com>
2019-12-09 23:50:34 +0100

    Use styled-components macro

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 import Deck from './Deck'
 import Stats from './Stats'
