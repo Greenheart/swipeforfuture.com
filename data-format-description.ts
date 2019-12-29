b8e9ae639ab366a508ac48f3a25745e39ad62d69
Mattias Nyberg <muthaias.uu@gmail.com>
2019-12-29 20:24:24 +0100

    * Data format description for cards and world
    Co-authored-by: Samuel Plumppu <greenheartmusic@gmail.com>

---
+++ data-format-description.ts
@@ -0,0 +1,74 @@
+type WorldState = {}
+
+interface CardDescription {
+    image: string,
+    title: string,
+    text: string,
+    weight: number,
+}
+
+interface CardActionData {
+    description?: string,
+    modifierType?: "add" | "set" | "replace",
+    modifier?: {
+        environment?: number,
+        people?: number,
+        security?: number,
+        money?: number
+    },
+    flags?: {
+        [x: string]: boolean
+    }
+}
+
+interface CardData extends CardDescription {
+    isIncludedWhen: WorldQuery[],
+    actions: {
+        left: CardActionData,
+        right: CardActionData,
+    }
+}
+
+type WorldStateRange = [number, number]
+
+interface WorldQuery {
+    state?: {
+        environment?: WorldStateRange,
+        people?: WorldStateRange,
+        security?: WorldStateRange,
+        money?: WorldStateRange,
+        [x: string]: WorldStateRange,
+    },
+    flags?: {
+        [x: string]: boolean
+    },
+}
+
+
+
+
+// -----------------------------
+
+interface WorldEvent {
+    probability: number,
+    shouldTriggerWhen?: WorldQuery[],
+    initialEventCardId: EventCardId,
+}
+
+type EventCards = {
+    [eventCardId: string]: EventCard
+}
+
+interface EventCard extends CardDescription {
+    actions: {
+        left: EventCardActionData,
+        right: EventCardActionData
+    }
+}
+
+type EventCardId = string
+
+interface EventCardActionData extends CardActionData {
+    nextEventCardId: EventCardId,
+}
+
