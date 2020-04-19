612f092334ca43202d62bc030d7f52a507cdfcf8
Samuel Plumppu <greenheartmusic@gmail.com>
2020-04-19 18:43:22 +0200

    Updates

--- precache-manifest.aee8ac59f15993c452bdd33447b74a6e.js
+++ precache-manifest.aee8ac59f15993c452bdd33447b74a6e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71e50cdc3241cab1d9a8ee39e343b3f4",
+    "revision": "7ca44792539ed30b936c13ba2b94637e",
     "url": "/index.html"
   },
   {
-    "revision": "12918a78197af087f6bf",
-    "url": "/static/css/main.ca9ec396.chunk.css"
+    "revision": "e11e0a3b460c625eeb7a",
+    "url": "/static/css/main.2b10f261.chunk.css"
   },
   {
     "revision": "d287445286b84fc303a4",
     "url": "/static/js/2.41c0944f.chunk.js"
   },
   {
-    "revision": "12918a78197af087f6bf",
-    "url": "/static/js/main.908c234b.chunk.js"
+    "revision": "e11e0a3b460c625eeb7a",
+    "url": "/static/js/main.4561c29b.chunk.js"
   },
   {
     "revision": "5071af73c86f45722960",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c446a45b15290b5d8e54f355e4bb7df1.js"
+  "/precache-manifest.aee8ac59f15993c452bdd33447b74a6e.js"
 );
 
 self.addEventListener('message', (event) => {
