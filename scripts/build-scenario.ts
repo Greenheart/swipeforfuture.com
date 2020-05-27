608b2817898189b2db88886b46c87c73a1c45dee
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-27 23:59:40 +0200

    feat(content API): Automate Scenario builds with a common interface and simple CLI
    - Add dedicated script to execute and build scenarios. This separation is clear from the content-utils
    - Add Scenario interface to show expected format and runtime
    - Improve `yarn scenario` to easily build default scenario, or any Scenario ID provided by CLI args
    
    This includes some hacks, but should provide a nice foundation to automate scenario builds.

---
+++ scripts/build-scenario.ts
@@ -0,0 +1,23 @@
+// Build a scenario with given ID from CLI args
+
+// Usage: `yarn scenario [ID]`
+// ID = name of scenario script OR 'default' if no ID provided
+
+const id = process.argv.length >= 3 ? process.argv[2] : 'default'
+
+console.log(`/scenarios/${id}.ts`)
+
+// HACK: export this internal interface to count this as an ES module, which is required to run this
+export interface Scenario {
+    run: () => void
+}
+
+// TODO: Add support for top level await by using the next version of es modules in tsconfig.json
+// This had to be changed to support ES modules in node scripts
+// but there must be some way to get both ES modules + top level await
+
+import(`./scenarios/${id}.ts`).then((scenario: Scenario) => {
+    const gameWorld = scenario.run()
+    console.log(gameWorld)
+    console.log(`"${id}" scenario was built!`)
+})
--- scripts/scenarios/road-to-utopia.ts
+++ scripts/scenarios/road-to-utopia.ts
@@ -1,7 +1,10 @@
 import { CardData } from '../../src/game/ContentTypes'
 
+// HACK: This useless import + function is required for this to count as an ES module, so we can run it
 function noop(c: CardData): CardData {
     return c
 }
 
-console.log('"road-to-utopia" scenario was built!')
+export function run() {
+    return 'A true utopian scenario, wow!'
+}
