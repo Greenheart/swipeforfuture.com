1d0334e0ce9bcd8cc34a20e208c8a7679fb90d74
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-28 00:15:06 +0200

    fix(content API): remove noop-code

--- scripts/scenarios/road-to-utopia.ts
+++ scripts/scenarios/road-to-utopia.ts
@@ -1,10 +1,3 @@
-import { CardData } from '../../src/game/ContentTypes'
-
-// HACK: This useless import + function is required for this to count as an ES module, so we can run it
-function noop(c: CardData): CardData {
-    return c
-}
-
 export function run() {
     return 'A true utopian scenario, wow!'
 }
