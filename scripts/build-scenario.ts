6b590c1e8700722e47f0502f4db45e049ab8937a
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-28 21:39:44 +0200

    fix(content API): Cleanup scenario builder
    Co-authored-by: Mattias Nyberg <muthaias.uu@gmail.com>

--- scripts/build-scenario.ts
+++ scripts/build-scenario.ts
@@ -3,16 +3,16 @@
 // Usage: `yarn scenario [ID]`
 // ID = name of scenario script OR 'default' if no ID provided
 
-import { Scenario, ScenarioBuilder } from './content-utils'
+import { ScenarioBuilder } from './content-utils'
 
 const id = process.argv.length >= 3 ? process.argv[2] : 'default'
 
-async function exportScenario(id: string) {
+async function buildScenario(id: string) {
     const builder: ScenarioBuilder = await import(`./scenarios/${id}.ts`)
-    const scenario: Scenario = builder.run()
+    const scenario = builder.run()
 
     console.log(scenario)
     console.log(`"${id}" scenario was built!`)
 }
 
-exportScenario(id)
+buildScenario(id)
