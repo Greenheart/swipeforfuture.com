068588d195cf2dfd212521d8a7811bf7af7cb968
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-28 19:57:44 +0200

    feature(content API): refactor scenario builder
    Co-authored-by: Mattias Nyberg <muthaias.uu@gmail.com>

--- scripts/build-scenario.ts
+++ scripts/build-scenario.ts
@@ -3,19 +3,16 @@
 // Usage: `yarn scenario [ID]`
 // ID = name of scenario script OR 'default' if no ID provided
 
+import { Scenario, ScenarioBuilder } from './content-utils'
+
 const id = process.argv.length >= 3 ? process.argv[2] : 'default'
 
-// HACK: export this internal interface to count this as an ES module, which is required to run this
-export interface Scenario {
-    run: () => void
+async function exportScenario(id: string) {
+    const builder: ScenarioBuilder = await import(`./scenarios/${id}.ts`)
+    const scenario: Scenario = builder.run()
+
+    console.log(scenario)
+    console.log(`"${id}" scenario was built!`)
 }
 
-// TODO: Add support for top level await by using the next version of es modules in tsconfig.json
-// This had to be changed to support ES modules in node scripts
-// but there must be some way to get both ES modules + top level await
-
-import(`./scenarios/${id}.ts`).then((scenario: Scenario) => {
-    const gameWorld = scenario.run()
-    console.log(gameWorld)
-    console.log(`"${id}" scenario was built!`)
-})
+exportScenario(id)
