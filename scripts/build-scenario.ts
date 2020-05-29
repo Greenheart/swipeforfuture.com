c55fe19e869cc64c56db595bd9800fb9711981ce
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-30 01:09:34 +0200

    feature(content API): Initial JSON export to build directory

--- scripts/build-scenario.ts
+++ scripts/build-scenario.ts
@@ -3,7 +3,10 @@
 // Usage: `yarn scenario [ID]`
 // ID = name of scenario script OR 'default' if no ID provided
 
-import { ScenarioBuilder } from './content-utils'
+import { ScenarioBuilder, Scenario } from './content-utils'
+import { promises } from 'fs'
+import { GameWorld } from '../src/game/ContentTypes'
+const { writeFile, mkdir } = promises
 
 const id = process.argv.length >= 3 ? process.argv[2] : 'default'
 
@@ -16,10 +19,55 @@ async function buildScenario(id: string) {
         const scenario = builder.run()
 
         console.log(JSON.stringify(scenario, null, 2))
-        console.log(`"${id}" scenario was built!`)
+        console.log(`✅ Sucessfully built scenario "${id}"`)
+
+        return scenario
     } catch (e) {
-        console.error(`Error: Failed to build scenario ${id}`, e)
+        throw new Error(`Error: Failed to build scenario "${id}" - ${e}`)
     }
 }
 
-buildScenario(id)
+async function exportScenario(id: string, scenario: Scenario) {
+    // TODO: export scenario to JSON
+    // Build into separate files and store in a output directory.
+    // Create files in a directory, or write to existing if they already have been created.
+
+    type Parts = {
+        [K in keyof GameWorld]: string
+    }
+
+    const scenarioParts: Parts = {
+        cards: 'cards',
+        eventCards: 'event-cards',
+        events: 'events',
+        defaultState: 'default-state',
+        stats: 'stats',
+    }
+
+    // Create the necessary directories if they are missing
+    const outputDir = `${__dirname}/build/${id}`
+    await mkdir(outputDir, { recursive: true })
+
+    await Promise.all(
+        Object.entries(scenarioParts).map(([part, fileName]) =>
+            writeFile(
+                `${outputDir}/${fileName}.json`,
+                JSON.stringify(scenario[part as keyof GameWorld], null, 4),
+            ),
+        ),
+    )
+
+    // TODO: improve the export, verify that it works correcly by diffing JSON output with default scenario
+    // TODO: Export directly to the /src/public/scenarios directory to make it playable in game
+    // TODO: Test export in the game and see if it's playable
+    // TODO: simplify prop references to make output cleaner, since data might be edited manually in other editors - and that the generated prop references will change on every build.
+
+    // IDEA: Add support for building all images used and saving them to a directory
+    // save with format `pexels-[ID].png` or `unsplash-[ID].png`
+
+    console.log(`✅ Sucessfully exported scenario "${id}"`)
+}
+
+buildScenario(id).then((scenario: Scenario) => {
+    exportScenario(id, scenario)
+})
