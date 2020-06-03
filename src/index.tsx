5e531857dcf613450a15e77e0e001cadc25959f2
Samuel Plumppu <greenheartmusic@gmail.com>
2020-06-03 08:05:57 +0200

    feature(content API): Load new default scenario from generated content

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import './index.css'
 import App from './App'
 
 const urlParams = new URLSearchParams(window.location.search)
-const path = urlParams.get('path') || '/data/default'
+const path = urlParams.get('path') || '/data/scenarios/default'
 
 ReactDOM.render(<App path={path} />, document.getElementById('root'))
