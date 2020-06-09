07a2970621138ac9c8ef683234ff8e0267865fdf
Samuel Plumppu <greenheartmusic@gmail.com>
2020-06-09 23:37:14 +0200

    fix(load scenario): Simplify scenario loading to remove old code.
    Also make sure we only start the game with fully loaded game worlds.
    The previous default values didn't make sense any more.
    They were mostly used for initial PoC back in late 2019.

--- src/game/load-scenario.ts
+++ src/game/load-scenario.ts
@@ -1,7 +1,3 @@
-import { cards as defaultCards } from '../data/default/cards'
-import { events as defaultEvents } from '../data/default/events'
-import { eventCards as defaultEventCards } from '../data/default/event-cards'
-
 import {
     WorldState,
     StatDefinition,
@@ -10,53 +6,6 @@ import {
     WorldEvent,
     GameWorld,
 } from './ContentTypes'
-
-export const DEFAULT_GAME_STATE: WorldState = Object.freeze({
-    state: {
-        environment: 40,
-        people: 60,
-        security: 75,
-        money: 90,
-    },
-    flags: {},
-})
-
-const defaultStats: StatDefinition[] = [
-    {
-        id: 'environment',
-        name: 'Environment',
-        icon: 'GiWheat',
-    },
-    {
-        id: 'people',
-        name: 'People',
-        icon: 'IoIosPeople',
-    },
-    {
-        id: 'security',
-        name: 'Security',
-        icon: 'GiAk47',
-    },
-    {
-        id: 'money',
-        name: 'Money',
-        icon: 'GiMoneyStack',
-    },
-]
-
-export const defaultGameWorld: GameWorld = {
-    stats: defaultStats,
-    cards: defaultCards,
-    events: defaultEvents,
-    eventCards: defaultEventCards,
-    defaultState: DEFAULT_GAME_STATE,
-}
-
-const internalGameWorlds: {
-    [worldId: string]: GameWorld
-} = {
-    default: defaultGameWorld,
-}
 
 async function tryLoadFromLocalStorage(
     path: string,
@@ -68,45 +17,36 @@ async function tryLoadFromLocalStorage(
         // Using localStorage to share data across apps
         const id = matchLocal[1]
         const gameWorldId = 'game_world:' + id
-        let gameWorld = {}
+        const gameWorld: GameWorld = {
+            stats: [],
+            cards: [],
+            events: [],
+            eventCards: {},
+            defaultState: {
+                state: {},
+                flags: {},
+            },
+        }
         try {
             const serializedData = localStorage.getItem(gameWorldId)
-            const data = serializedData ? JSON.parse(serializedData) : null
+            const data: GameWorld = serializedData
+                ? JSON.parse(serializedData)
+                : null
             if (!data)
                 throw new Error(
                     'Could not load data from local storage: ' + gameWorldId,
                 )
-            gameWorld = data || {}
+            return Object.assign(gameWorld, data)
         } catch (e) {
             console.log(e)
         }
-        return Object.assign({}, defaultGameWorld, gameWorld)
-    }
-
-    return null
-}
-
-// Load GameWorld from data directory of this repository
-async function tryLoadFromInternalData(
-    path: string,
-): Promise<GameWorld | null> {
-    const matchInternal = path.match(/^internal:\/\/(.*)/)
-
-    if (matchInternal) {
-        const id = matchInternal[1]
-        const gameWorld = internalGameWorlds[id]
-        if (!gameWorld) {
-            console.warn('Could not load local game world: ', id)
-            return defaultGameWorld
-        }
-        return gameWorld
     }
 
     return null
 }
 
 async function tryLoadFromRestAPI(path: string): Promise<GameWorld | null> {
-    // Default: expect a folder to represent a game world and contain specific JSON-files.
+    // Default: expect a folder to represent a GameWorld and contain specific JSON-files.
     const statsPath = path + '/stats.json'
     const cardsPath = path + '/cards.json'
     const eventsPath = path + '/events.json'
@@ -120,11 +60,11 @@ async function tryLoadFromRestAPI(path: string): Promise<GameWorld | null> {
         EventCards,
         WorldState
     >([
-        fetchJSON(statsPath, defaultStats),
-        fetchJSON(cardsPath, []),
-        fetchJSON(eventsPath, []),
-        fetchJSON(eventCardsPath, {}),
-        fetchJSON(defaultStatePath, DEFAULT_GAME_STATE),
+        fetchJSON(statsPath),
+        fetchJSON(cardsPath),
+        fetchJSON(eventsPath),
+        fetchJSON(eventCardsPath),
+        fetchJSON(defaultStatePath),
     ])
 
     return {
@@ -138,19 +78,16 @@ async function tryLoadFromRestAPI(path: string): Promise<GameWorld | null> {
 
 export async function loadScenario(path: string) {
     return (
-        (await tryLoadFromInternalData(path)) ||
         (await tryLoadFromLocalStorage(path)) ||
         (await tryLoadFromRestAPI(path))
     )
 }
 
-async function fetchJSON<T>(path: string, defaultValue: T): Promise<T> {
+async function fetchJSON<T>(path: string): Promise<T> {
     try {
         console.log('fetching path: ', path)
         return await (await window.fetch(path)).json()
     } catch (e) {
-        console.warn('Could not load path: ' + path)
-
-        return defaultValue
+        throw new Error('Could not load path: ' + path)
     }
 }
