1bde947ad90abec2d86daef6b81d21327191577b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-08-10 20:52:08 +0200

    Further improve responsiveness for smallest screens down to 320x480 px (iPhone 4 / 2010)

--- src/components/CardView.svelte
+++ src/components/CardView.svelte
@@ -9,13 +9,13 @@
     export let onSwipe: (direction: SwipeDirection) => Promise<void>
 
     const imageSize =
-        'w-[85vmin] h-[85vmin] max-w-[400px] max-h-[400px] rounded-lg'
+        'w-[60vmin] h-[60vmin] 2xs:w-[85vmin] 2xs:h-[85vmin] max-w-[400px] max-h-[400px] rounded-lg'
 </script>
 
 <div
-    class="flex flex-col items-center justify-center text-white xs:text-lg font-light max-w-prose m-auto"
+    class="flex flex-col items-center justify-center text-white 2xs:text-lg xs:text-xl font-light max-w-prose m-auto"
 >
-    <p class="text-center py-4 h-48 xs:h-64 flex place-items-center px-4 xs:px-8">{card.text}</p>
+    <p class="text-center xs:py-4 h-36 2xs:h-40 xs:h-56 flex place-items-center px-4 xs:px-8">{card.text}</p>
     <div class="{imageSize} relative">
         <div class="absolute z-0 {imageSize} top-0 left-0 bg-gray-900" />
         <Card {onSwipe}>
@@ -25,6 +25,6 @@
             />
         </Card>
     </div>
-    <h2 class="mt-8">{card.title}</h2>
-    <p class="text-gray-300">{card.location}</p>
+    <h2 class="mt-2 xs:mb-4">{card.title}</h2>
+    <p class="text-gray-300 mb-2 xs:mt-4">{card.location}</p>
 </div>
