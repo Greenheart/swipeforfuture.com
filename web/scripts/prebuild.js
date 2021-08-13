b0113e39f01f52c6ec07b25faa050ddb5b273277
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-08-13 16:26:07 +0200

    Simplify the prebuild script to use native nodejs rm() method instead of fs-extra wrapper

--- web/scripts/prebuild.js
+++ web/scripts/prebuild.js
@@ -1,7 +1,7 @@
-import { remove } from 'fs-extra'
+import { rm } from 'fs/promises'
 import { resolve } from 'path'
 
 /**
  * Don't deploy development versions of scenarios together with the game client.
  */
-await remove(resolve('./static/dev-only'))
+await rm(resolve('./static/dev-only'), { recursive: true, force: true })
