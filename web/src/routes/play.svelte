3ee23839ac4a872b06839ca5d56b7a28908cc2f0
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-08-13 23:04:10 +0200

    Better alignment for loading text

--- web/src/routes/play.svelte
+++ web/src/routes/play.svelte
@@ -35,7 +35,7 @@
     {/await}
 {:else}
     <p
-        class="grid place-items-center mt-40 text-white font-black text-2xl tracking-widest"
+        class="grid place-items-center text-white font-black text-2xl tracking-widest h-[60vh]"
     >
         Loading...
     </p>
