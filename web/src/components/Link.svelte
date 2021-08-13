38d2ed4ba7bee8fcf301e836bdf873f5327c4f3a
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-08-13 21:39:39 +0200

    Add Link component for easy internal/external links

---
+++ web/src/components/Link.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let href = ''
+    export let external = false
+    const additionalProps = external ? { rel: 'noopener noreferrer', target: '_blank' } : {'sveltekit:prefetch': true }
+</script>
+
+<a {href} {...$$props} {...additionalProps}>
+    <slot />
+</a>
