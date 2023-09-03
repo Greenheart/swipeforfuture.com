35d5be1ef7ccc7c279de3e1e41fe30627d766559
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-09-03 18:18:30 +0200

    Cleanup

--- content/loader.js
+++
@@ -1,7 +0,0 @@
-import { isBuiltin } from "node:module"
-
-
-// noinspection JSUnusedGlobalSymbols
-export const resolve = (specifier, context, nextResolve) =>
-  nextResolve(isBuiltin(specifier) || specifier.endsWith('.js') ? specifier : `${specifier}.js`, context)
-
