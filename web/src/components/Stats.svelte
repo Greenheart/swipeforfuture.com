d54f5e18a723c5b81204b30cbd01c5071516ca0e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-09-09 13:59:41 +0200

    Add title to stats to show the full name

--- web/src/components/Stats.svelte
+++ web/src/components/Stats.svelte
@@ -13,7 +13,10 @@
     class="flex bg-green-700 xs:space-x-10 justify-around xs:justify-center items-center p-2 2xs:p-4 shadow-lg"
 >
     {#each stats as stat}
-        <div class="flex justify-between items-center flex-col">
+        <div
+            class="flex justify-between items-center flex-col"
+            title={stat.name}
+        >
             <div
                 class="w-8 h-8 2xs:w-10 2xs:h-10 xs:w-12 xs:h-12 rounded-full bg-white grid place-items-center shadow-lg mb-2 xs:mb-3 text-black text-sm"
             >
